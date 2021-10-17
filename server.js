adfb2fc4e71c70dc48e779830d9cd9b414d8c5f2
Lavish Goyal <goellavish10@gmail.com>
2021-10-17 16:46:32 +0530

    setting up folder structure

--- server.js
+++ server.js
@@ -10,4 +10,10 @@ app.get("/", (req, res) => {
   res.send("API RUNNING");
 });
 
+// Define Routes
+app.use("/api/users", require("./routes/API/users"));
+app.use("/api/auth", require("./routes/API/auth"));
+app.use("/api/profile", require("./routes/API/profile"));
+app.use("/api/posts", require("./routes/API/posts"));
+
 app.listen(PORT, () => console.log(`Server is up and running at Port ${PORT}`));
